a0d16d9ffccee8e13c7a33935850ae803ad59100
Niharika Menon <080300nic@gmail.com>
2021-12-01 17:29:02 +0530

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,8 @@
         }
     });
 
+
+
     // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
