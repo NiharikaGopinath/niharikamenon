bff6030b00d50d18a2de1067f4d97af059199fe5
Niharika Menon <080300nic@gmail.com>
2021-12-03 17:51:05 +0530

    update

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,11 @@
 
 
 
+
+   
+
+
+
     // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
